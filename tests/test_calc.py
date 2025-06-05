78be9dc4944f6f252d2f33c4a81ae4734df0e51b
minhokiller <andresuave.sc@gmail.com>
2025-06-05 20:34:11 -0300

    adicionado teste do multiplica

--- tests/test_calc.py
+++ tests/test_calc.py
@@ -1,7 +1,10 @@
-from app.calc import soma, subtrai
+from app.calc import soma, subtrai, multiplica
 
 def test_soma():
     assert soma(2,3) == 5
 
 def test_subtrai():
     assert subtrai(8,5) == 3
+
+def test_multiplica():
+    assert multiplica(2,5) == 10
